2f7a377919ece7f3dcdbe608cda5ecec08b6405c
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-11-06 13:17:09 +0100

    python list

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<Python.h>
+
+/**
+ * print_python_list_info - object info
+ * @p: object
+ * Return: void.
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, alloc, idx;
+
+	size = PyList_Size(p);
+	alloc = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", alloc);
+	for (idx = 0; idx < size; idx++)
+	{
+		printf("Element %ld: %s\n",
+		       idx,
+		       (PY_TYPE(PyList_GetItem(p, idx)))->tp_name);
+	}
+}
