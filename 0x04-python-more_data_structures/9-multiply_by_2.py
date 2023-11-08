7bac036603d6914b7c5ee977ac35935dfb80e85f
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-11-08 21:37:14 +0100

    10-best_score.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    if a_dictionary:
+    if a_dictionary is not None:
         dict = {}
         tmp = {}
         for key, value in a_dictionary.items():
