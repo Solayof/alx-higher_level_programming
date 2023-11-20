fede7da4368e375fe81d8986eccfa150ae3f19ed
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-11-20 20:49:34 +0100

    advance

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[idx]), end="")
             i += 1
         except (TypeError, ValueError):
-            return False
+            continue
     print("")
     return i
