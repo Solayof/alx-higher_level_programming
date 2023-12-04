4c5df375fd8f7e96a4a5fe61d87a89e8be06786e
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-12-04 16:37:48 +0100

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module 0-lookup.py
+
+this module contains method that return list of object's attributes and methods
+
+"""
+
+def lookup(obj):
+    """Return object attributes and method"""
+    return dir(obj)
