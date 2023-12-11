f39cc46fe0dc277caa95516f305a25b810785f6f
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-12-11 07:31:10 -0800

    indentation Error

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,49 +27,49 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
         self.size = size
-@property
-        def size(self):
-            """Get the size of a Square class"""
-            return self.height
+    @property
+    def size(self):
+        """Get the size of a Square class"""
+        return self.height
 
-        @size.setter
-        def size(self, val):
-            self.width = val
-            self.height = val
+    @size.setter
+    def size(self, val):
+        self.width = val
+        self.height = val
 
-        def __str__(self):
-            """define the string representation"""
-            return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+    def __str__(self):
+        """define the string representation"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
 
-        def update(self, *args, **kwargs):
-            """updates Rectangle Class attributes"""
-            if args:
-                for k, v in enumerate(args):
-                    if k == 0:
-                        self.id = v
-                    if k == 1:
-                        self.size = v
-                    if k == 2:
-                        self.x = v
-                    if k == 3:
-                        self.y = v
-            else:
-                if "id" in kwargs:
-                    self.id = kwargs["id"]
-                if "size" in kwargs:
-                    self.width = kwargs["size"]
-                if "x" in kwargs:
-                    self.x = kwargs["x"]
-                if "y" in kwargs:
-                    self.y = kwargs["y"]
+    def update(self, *args, **kwargs):
+        """updates Rectangle Class attributes"""
+        if args:
+            for k, v in enumerate(args):
+                if k == 0:
+                    self.id = v
+                if k == 1:
+                    self.size = v
+                if k == 2:
+                    self.x = v
+                if k == 3:
+                    self.y = v
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.width = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
 
-        def to_dictionary(self):
-            """Return dictionary representation"""
-            dic_ = {}
-            dic_["id"] = self.id
-            dic_["size"] = self.size
-            dic_["x"] = self.x
-            dic_["y"] = self.y
-            return dic_
+    def to_dictionary(self):
+        """Return dictionary representation"""
+        dic_ = {}
+        dic_["id"] = self.id
+        dic_["size"] = self.size
+        dic_["x"] = self.x
+        dic_["y"] = self.y
+        return dic_
 
         
