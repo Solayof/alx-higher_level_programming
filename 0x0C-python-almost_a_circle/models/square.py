d7721587d75ef55da7e5b2218b3fe07fd9de6dec
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2023-12-11 06:39:19 -0800

    module impotation modified

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -43,7 +43,7 @@ class Square(Rectangle):
 
         def update(self, *args, **kwargs):
             """updates Rectangle Class attributes"""
-        if args:
+        if args is not None:
             for k, v in enumerate(args):
                 if k == 0:
                     self.id = v
