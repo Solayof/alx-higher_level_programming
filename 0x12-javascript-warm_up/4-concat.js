f8b490f96efec1f531606609165e6bb7764fa20a
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-02-12 14:54:43 +0100

    corerect task 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -4,7 +4,7 @@
  * store the second arg to a variable
  *
  */
-const firstArg = processs.argv[2];
+const firstArg = process.argv[2];
 const  secondArg = process.argv[3];
 
 console.log(`${firstArg} is ${secondArg}`);
