1987cacf3bc55f14e3a1e1eff3bb970a8d76d3f9
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-02-13 20:47:47 +0100

    task 8: Created

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,13 +2,13 @@
 
 const ParentSquare = require('./5-square');
 module.exports = class Square extends ParentSquare {
-	constructor (size) {
-		super(size, size);
-	}
+  constructor (size) {
+    super(size, size);
+  }
 
-	charPrint(c = "X") {
-		for  (let idx = 0; idx < this.height; idx++) {
-			console.log(c.repeat(this.width));
-		}
-	}
-}
+  charPrint (c = 'X') {
+    for (let idx = 0; idx < this.height; idx++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
