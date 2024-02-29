e369a2f6c50d83300fb278b4ed99859e1a9c35da
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-03-01 00:53:50 +0100

    task 6 efficiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
-"""Function that finds a peak in a list of unsorted integers"""
+"""Function that finds a peak in a list of unsorted integers
+"""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in list_of_integers"""
-
-    if list_of_integers is None or list_of_integers == []:
+    if not list_of_integers:
         return None
-    else:
-        return max(list_of_integers)
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        # Check if mid is a peak
+        if (mid == 0 or list_of_integers[mid] >= list_of_integers[mid - 1])\
+                and (mid == len(list_of_integers) - 1
+                     or list_of_integers[mid] >= list_of_integers[mid + 1]):
+            return list_of_integers[mid]
+
+        # Move towards the direction of the greater neighbor
+        if mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return None
