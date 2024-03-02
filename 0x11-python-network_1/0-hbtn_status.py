9c3bd70c4322bdd73fe4c6ddfaa168c43df74b53
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-03-02 01:02:52 +0100

    pycodestyle fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@ fetch https://intranet.hbtn.io/status; display response
 import urllib.request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        html = res.read()
+        print('Body res:')
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
