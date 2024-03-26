50049910430c63e5177768371f85067767c35f23
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-03-26 18:45:47 +0000

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let id = process.argv[2];
+let url = "http://swapi/api/films/" + id;
+rqst = reqiure("request");
+rqst.get(url, (err,  resp, body) => {
+    if (err) {
+        console.log(err);
+    } else if (resp.statusCode === 200) {
+        body = JSON.parse(body);
+        console.log(body["title"]);
+    } else {
+        console.log(`Error code: ${resp.statusCode}`);
+    }
+});
