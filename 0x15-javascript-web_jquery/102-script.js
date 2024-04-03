df103bc3cb72b63f3180aaee70e21ac804828880
Moses Solomon Ayofemi <solomonayofemi@gmail.com>
2024-04-03 22:57:07 +0100

    task 102

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,6 +1,6 @@
 $(function () {
   $('INPUT#btn_translate').click(function () {
-    $.get('https://www.fourtonfish.com/hellosalut/hello/' + $('INPUT#language_code').val() + '%22)&format=json', function (resp, status) {
+    $.get('https://www.fourtonfish.com/hellosalut/hello/' + $('INPUT#language_code').val() + '?format=json', function (resp, status) {
       if (status === 'success') {
         $('DIV#hello').text(resp.hello);
       }
